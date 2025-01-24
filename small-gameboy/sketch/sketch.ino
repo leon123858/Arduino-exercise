581c10165a56bf3b12151334ddaf48e3fcb1f501
leon123858 <a0970785699@gmail.com>
2025-01-24 09:43:53 +0800

    update

--- small-gameboy/sketch/sketch.ino
+++ small-gameboy/sketch/sketch.ino
@@ -4,16 +4,26 @@
 
 #define IS_DEBUG false
 
+/*
+ * This line initializes a pointer to Controller class object, which will control system functions.
+ */
 Controller *controller = nullptr;
 
+/**
+ * Initialize function called once when the program starts
+ */
 void setup()
 {
 #if (IS_DEBUG)
+	// Initializes Serial communication at baud rate of 9600 for debugging purposes
 	Serial.begin(9600);
 #endif
 	controller = new Controller();
 }
 
+/**
+ * Main loop that runs continuously while the program is running
+ */
 void loop()
 {
 	controller->Serve();
